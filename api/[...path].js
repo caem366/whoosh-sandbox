3a723c341280858cbc66d8511af7682198be4624
Caelan <caelanmason1@gmail.com>
2026-09-04 15:23:14 -0400

    Route all API paths through Vercel function

--- api/[...path].js
+++ api/[...path].js
@@ -1,1 +1,12 @@
-module.exports = require('./app.cjs').app
+const app = require('./app.cjs').app
+
+module.exports = (request, response) => {
+  const url = new URL(request.url, 'http://localhost')
+  const originalPath = url.searchParams.get('__whoosh_path')
+
+  if (originalPath) {
+    request.url = `/api/${originalPath}`
+  }
+
+  return app(request, response)
+}
